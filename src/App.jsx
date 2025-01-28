5ca82fcd5780e556d87741b64129c18e8c5f56a3
Júlia Marinho <julia.marinho@3stecnologia.com.br>
2025-01-28 14:09:04 -0300

    Arquivos iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Banner from './componentes/Banner/Banner.jsx'
+
+
+function App() {
+    return (
+        <div className="App">
+            <Banner />
+        </div>
+    );
+}
+
+export default App;
